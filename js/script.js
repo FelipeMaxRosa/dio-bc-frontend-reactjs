51623c3e3db2967be9ab76680559e9ac19e538b4
FelipeMaxRosa <felipemax.suporteti@gmail.com>
2020-04-17 10:10:55 -0300

    criado a cobrinha

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;   // 32px cada quadradinho
+let snake = [];
+snake[0] = {
+  x: 8 * box, 
+  y: 8 * box
+}
+
+
+// FUNÇÕES
+// -----------------------------------------------
 
 function criarBG(){
   context.fillStyle = "lightgreen";
-  context.fillRect(0,0,16 * box, 16 * box);
+  context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobrinha(){
+  for(i=0; i < snake.length; i++){
+    context.fillStyle = "green";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+    console.log(i);
+  }
 }
 
 criarBG();
+criarCobrinha();
